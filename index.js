386ca67064ec69296f561c99cc3705e869c02daa
Daniel Rice <danielrice5627@gmail.com>
2022-11-14 10:07:20 -0800

    commit tree 347b4409e86bd2fe25bb987a5660dcfdbf24d0fa

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function finder(array) {
+   return array.result===`W`;
+}
+
+function superbowlWin(array) {
+   let winYear = array.find(finder);
+   if (winYear===undefined){
+    return undefined
+} else {
+    return winYear.year
+}
+}
